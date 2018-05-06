123ebb619524cbbf822596671a5d13b82cfd31ed
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2018-05-06 21:41:23 +0200

    Remove option args from print script

--- print.sh
+++ print.sh
@@ -9,7 +9,7 @@ inotifywait -m --timefmt '%d/%m/%y %H:%M:%S' --format '%T %w %f' \
      FILECHANGE=${dir}${file}
      # convert absolute path to relative
      FILECHANGEREL=`echo "$FILECHANGE" | sed 's_'$CURPATH'/__'`
-     lp -o landscape -o fit-to-page -o media=A6 $FILECHANGEREL
+     lp -o landscape $FILECHANGEREL
 
      echo "At ${time} on ${date}, file $FILECHANGE was printed"
 done
